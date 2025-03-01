fedcbd28cf80a95866777f2cfba2dfe9ad6484cb
runnywolf <71980953+runnywolf@users.noreply.github.com>
2025-03-01 18:07:49 +0800

    v0.1.0-dev.17 (split exam paper from ep)

--- src/views/ExamView.vue
+++ src/views/ExamView.vue
@@ -142,70 +142,28 @@
 		</div>
 		<div class="column is-fluid">
 			<div class="ts-box">
-				<div
-					class="ts-content problem-font exam"
-					:style="{ filter: 'blur('+ (isProblemVisible ? 0 : 10) +'px)'}"
-				><!-- 模糊化題本 -->
-					<template v-for="(problemId, i) in examData.problemCompId">
-						<ol v-if="problemId[0] !== '-'"
-							:style="{ 'padding-left': (11+9*problemId.length)+'px' }"
-							:start="problemId"
-						>
-							<li><!-- ol: 根據題目編號的長度修正 ol 的 padding-left -->
-								<component :is="problemAsyncComp[i]"></component>
-							</li>
-						</ol>
-						<div v-else><!-- 題號開頭若為 '-', 會隱藏題號 -->
-							<component :is="problemAsyncComp[i]"></component>
-						</div>
-						<div v-if="i != examData.problemCompId.length - 1" class="ts-divider is-section"></div><!-- 題目間的分隔線 -->
-					</template>
-				</div>
-				<div v-if="!isProblemVisible" class="ts-wrap is-vertical is-middle-aligned exam-ready"><!-- 開始作答的 ui, 覆蓋於模糊的題目之上 -->
-					<div class="ts-text is-huge is-bold"><!-- 題本年份 -->
-						{{ config[uni].shortName }}&nbsp;&nbsp;{{ examData.year }}
-					</div>
-					<div class="ts-list is-unordered"><!-- 考試建議 -->
-						<div class="item">
-							快速看過全部題目後再開始作答，考試時間為 {{ Math.floor(examTimeSec / 60) }} 分鐘，請做好配速。
-						</div>
-						<div class="item">如果有給手寫答案紙，且題本沒有附註「只寫答案即可」，建議附上運算過程。</div>
-						<div class="item">簡單題目請先做完，若時間夠，驗算一遍後再做剩餘題目。</div>
-					</div>
-					<button class="ts-button is-start-icon" @click="clickStartExam">
-						<span class="ts-icon is-pen-icon"></span>
-						開始作答
-					</button>
-					<div class="ts-text is-small is-secondary is-italic">
-						如果你只想翻閱一下歷屆試題，可以關閉左側選單中的「測驗模式」
-					</div>
-				</div>
+				<exam-paper
+					:uni="uni"
+					:examData="examData"
+					:isProblemVisible="isProblemVisible"
+					:examTimeSec="examTimeSec"
+					@clickStartExam="clickStartExam"
+				></exam-paper>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script setup>
-import { ref, shallowRef, watch, defineAsyncComponent } from "vue";
+import { ref, watch } from "vue";
 import config from "@/components/exam/config.json"; // 保存題本資訊的設定檔
-import NotFoundComp from "@/components/exam/NotFound.vue"; // 題目載入失敗時, 顯示的錯誤訊息組件
+import ExamPaper from "../components/exam-view/ExamPaper.vue"; // 考卷的組件 (於 v0.1.0-dev.17 分離)
 
 const uni = ref("ntu"); // 選取的學校
 const examData = ref(config[uni.value].exam[0]); // 選取的題本的資料
-const problemAsyncComp = shallowRef([]); // 目前顯示的題目組件. shallowRef 只有 .value 改變時更新元素
 
-const loadProblemComp = (id) => defineAsyncComponent( // 異步載入編號為 id 的題目組件
-	() => import(`../components/exam/${uni.value}/${examData.value.year}/${id}.vue`) // 載入題目組件
-		.catch(() => NotFoundComp) // 題目組件載入失敗時, 顯示錯誤訊息組件
-);
-watch(examData, async (newExamData) => { // 如果選取的題本年份改變了, 要做的事
-	problemAsyncComp.value = await Promise.all( // 並行載入所有題目組件
-		newExamData.problemCompId.map(loadProblemComp) // 異步載入所有題目
-	);
-}, { immediate: true }); // 頁面載入時, 載入一次題目
-
-const isExamModeEnabled = ref(true); // 是否開啟測驗模式, 預設為開啟
-const isProblemVisible = ref(false); // 是否要顯示題本內容. 注意此變數與 "正在作答" 等價
+const isExamModeEnabled = ref(false); // 是否開啟測驗模式, 預設為開啟
+const isProblemVisible = ref(!isExamModeEnabled.value); // 是否要顯示題本內容. 注意此變數與 "正在作答" 等價
 const isTimerActive = ref(false); // 計時器是否正在計時
 const examTimeSec = ref(6000); // 考試時間, 幾乎都是 100 分鐘, 師大 90 分鐘
 const remainingSec = ref(examTimeSec.value); // 計時器剩餘的秒數
@@ -284,18 +242,5 @@ const clickDownload = () => { // 下載題本
 }
 .sidebar-link-text:hover {
 	color: #f3f;
-}
-.exam > ol {
-	padding-left: 20px; /* 題目編號與 box 左側邊緣的距離 */
-}
-.exam > ol > li {
-	padding-left: 4px; /* 題目編號與題目的距離 */
-}
-.exam-ready { /* 開始作答的 ui, 覆蓋於模糊的題目之上 */
-	position: absolute; /* 絕對定位 */
-	top: 100px; /* 與考卷頂部的距離 */
-	left: 50%; /* 水平置中 */
-	transform: translate(-50%, 0); /* 調整 x 座標讓其水平置中 */
-	white-space: nowrap; user-select: none; /* 禁止換行, 禁止被選取 */
 }
 </style>
