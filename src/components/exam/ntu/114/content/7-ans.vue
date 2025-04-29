0148f39db0e7963daacbd9cd5bc0d9ff3bd0d757
runnywolf <71980953+runnywolf@users.noreply.github.com>
2025-04-29 17:45:58 +0800

    v0.3.1-dev.3 (MultiOption)
    Replace OptionAnswer with MultiOption.

---
+++ src/components/exam/ntu/114/content/7-ans.vue
@@ -0,0 +1,13 @@
+<template>
+	<MultiOption
+		:optionNames="['A', 'B', 'C', 'D', 'E']"
+		:optionAnswers="[true, false, true, false]"
+	>
+		<template #A>
+			a
+		</template>
+		<template #C>
+			c
+		</template>
+	</MultiOption>
+</template>
