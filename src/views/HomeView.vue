c21c064389a20874559d31152467fdc509335e5c
runnywolf <71980953+runnywolf@users.noreply.github.com>
2025-05-06 17:57:47 +0800

    deploy test 4

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
 		<div class="ts-content is-very-padded">
 			<div class="ts-wrap is-vertical is-middle-aligned is-compact">
 				<span class="ts-icon is-hammer-icon is-huge"></span>
-				<div class="ts-text is-huge">deploy test 3&nbsp;&nbsp;ฅ^⦁⩊⦁^ฅ ੭</div>
+				<div class="ts-text is-huge">deploy test 4&nbsp;&nbsp;ฅ^⦁⩊⦁^ฅ ੭</div>
 			</div>
 		</div>
 	</div>
