37c38ac3c827affcc2f168a5d33e22d9a33186ed
runnywolf <71980953+runnywolf@users.noreply.github.com>
2025-05-21 17:35:58 +0800

    v0.3.3-dev.6 (move katex style to comp vl)

--- src/components/problem/Vl.vue
+++ src/components/problem/Vl.vue
@@ -1,5 +1,5 @@
 <template>
-	<VueLatex :expression="exp" :displayMode="c" />
+	<VueLatex :expression="exp" :displayMode="c" class="vue-latex" />
 </template>
 
 <script setup> // 壓縮 vatex 標籤字符數的組件
@@ -10,3 +10,10 @@ const props = defineProps({
 	c: { type: Boolean, default: false } // 是否置中
 });
 </script>
+
+<style scoped>
+.vue-latex:deep(.katex) { /* latex 文字的 override style */
+	white-space: nowrap; /* 禁止換行 */
+	margin: 0 4px; /* 讓 latex 標籤與內文的間距再大一點 */
+}
+</style>
