59636168fbe0c9905d3acaed7efe61911a6d1a4e
runnywolf <71980953+runnywolf@users.noreply.github.com>
2025-06-30 19:05:18 +0800

    v0.3.6-dev.1 (refactor recur-input part1)
    Add Input ui

--- src/views/practice-page/RecurView.vue
+++ src/views/practice-page/RecurView.vue
@@ -1,4 +1,21 @@
 <template>
+	<div class="ts-text is-big is-bold">計算齊次 / 非齊次遞迴的一般式 ( 閉合形式 )</div>
+	<div class="ts-list is-unordered" style="margin-top: 4px;">
+		<div class="item">
+			按下 [ 隨機生成 ] 的按鈕可以生成特定情況的遞迴式，用於練習。
+		</div>
+		<div class="item">
+			下方的係數輸入框可以輸入整數，以及形式為 <code>a/b</code> 或 <code>x.xx</code> 的有理數。
+		</div>
+		<div class="item">
+			輸入框 n^ 只能輸入 0 ~ 3 的整數。
+		</div>
+	</div>
+	<div class="ts-divider is-section"></div>
+	<RecurInputNew></RecurInputNew>
+	<div class="ts-divider is-section"></div>
+	
+	
 	<RecurInput :recurLatex="recurLatex" @input="(d) => { recurData = d; }"></RecurInput>
 	<div class="ts-divider is-section"></div>
 	<Recur
@@ -9,6 +26,10 @@
 </template>
 
 <script setup>
+import RecurInputNew from "./recur-comp/RecurInputNew.vue"; // 遞迴生成器的參數設定
+
+// refactor line
+
 import { computed, ref, watch } from "vue";
 import RecurInput from "./recur-comp/RecurInput.vue"; // 遞迴輸入與隨機產生器
 import Recur from "@/components/answer-generator/Recur.vue"; // 遞迴解答生成器
---
+++ src/views/practice-page/recur-comp/RandomSetting.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="ts-wrap is-compact is-middle-aligned">
+		<!-- 隨機生成按鈕, 按下後會生成一個隨機遞迴 -->
+		<button class="ts-button is-outlined" @click="clickSubmit">隨機生成</button>
+		
+		<!-- 數字範圍 selector -->
+		<div class="ts-select is-solid">
+			<select v-model="numberRange">
+				<option value="3">數字 ± 3&nbsp;&nbsp;以內</option>
+				<option value="6">數字 ± 6&nbsp;&nbsp;以內</option>
+				<option value="10">數字 ± 10&nbsp;&nbsp;以內</option>
+			</select>
+		</div>
+		
+		<!-- 特徵根的類型 selector -->
+		<div class="ts-select is-solid">
+			<select v-model="rootType">
+				<option value="int">整數</option>
+				<option value="frac">分數</option>
+				<option value="root">帶有根號</option>
+				<option value="complex">複數</option>
+			</select>
+		</div>
+		
+		<span>的遞迴特徵根，以及整數的非齊次部分和初始條件。</span>
+		
+	</div>
+</template>
+
+<script setup> // 本組件的功能為: 調整遞迴生成器的參數
+import { ref } from 'vue';
+
+const emit = defineEmits([ "submit" ]); // 上傳隨機生成的模式
+
+const numberRange = ref("3"); // 數字範圍 (隨機生成的值可以使用的數字的範圍)
+const rootType = ref("int"); // 特徵根的類型
+
+const clickSubmit = () => { // 按下隨機生成的按鈕
+	emit("submit", { numberRange: Number(numberRange.value), rootType: rootType.value }); // 上傳隨機模式至 RecurInput.vue
+};
+</script>
