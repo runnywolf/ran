89225766f7913715e81b7ae62ff8ec02eabeccce
runnywolf <71980953+runnywolf@users.noreply.github.com>
2025-07-07 19:57:17 +0800

    v0.4-dev.8 (refactor Problem & ProblemView)

--- src/components/problem/Problem.vue
+++ src/components/problem/Problem.vue
@@ -15,32 +15,32 @@
 			</Content>
 			
 			<!-- 詳解連結-->
-			<RanLink class="problem-view-link" :to="`#/exam/${uni}-${year}/${no}`" newTab>
+			<RanLink class="problem-view-link" :to="`#/exam/${uni}-${year}/${no}`">
 				詳解
 			</RanLink>
 			
 		</div>
 		
 		<!-- 若顯示模式為 2, 會顯示綠框答案和詳解 -->
-		<div v-else-if="false" class="ts-wrap is-compact is-vertical content">
+		<div v-else-if="isContentMode()" class="ts-wrap is-compact is-vertical content">
 			
 			<!-- 答案 -->
 			<Content colorStyle="green" collapsed>
 				Ans:&nbsp;&nbsp;<vl :exp="answerLatex" />
 			</Content>
 			
-			<template v-for="(contentData, i) in problemConfig.content">
+			<template v-for="(contentConfig, i) in problemConfig.contentConfigs">
 				
 				<!-- 詳解類型的內容區塊 -->
-				<Content v-if="contentData.type === 'answer'" colorStyle="blue">
-					<details class="ts-accordion" name="problem-answer">
-						<summary>詳解 {{ contentData.suffix }}</summary>
+				<Content v-if="contentConfig.type === 'answer'" colorStyle="blue">
+					<details class="ts-accordion">
+						<summary>詳解 {{ contentConfig.suffix }}</summary>
 						<component :is="contentAsyncComps[i]"></component><!-- 詳解 -->
 					</details>
 				</Content>
 				
 				<!-- 預設的內容區塊 -->
-				<Content v-else-if="contentData.type === 'default'">
+				<Content v-else-if="contentConfig.type === 'default'">
 					<component :is="contentAsyncComps[i]"></component>
 				</Content>
 				
@@ -48,14 +48,14 @@
 				<Content v-else colorStyle="red">錯誤的內容區塊類型 ٩(ŏ﹏ŏ、)۶</Content>
 				
 			</template>
+			
 		</div>
 		
 	</div>
 </template>
 
 <script setup>
-import { defineAsyncComponent, computed } from "vue";
-import { useRouter } from "vue-router";
+import { defineAsyncComponent, computed, watch, shallowRef } from "vue";
 import LoadingComp from "./problem-comp/Loading.vue"; // 題目加載組件
 import SectionNotFoundComp from "./problem-comp/SectionNotFound.vue"; // 區塊載入失敗時, 顯示的錯誤訊息組件
 import ContentNotFoundComp from "./problem-comp/ContentNotFound.vue"; // 題目的內容載入失敗時, 顯示的錯誤訊息組件
@@ -71,6 +71,7 @@ const props = defineProps({
 // #region 載入題目組件
 const sectionAsyncComp = computed(() => { // 區塊組件 (題目或者是題本的說明區塊)
 	const { uni, year, no } = props;
+	if (!uni || !year || !no) return;
 	return defineAsyncComponent({ // 動態載入區塊組件
 		loader: () => import(`../../exam-db/${uni}/${year}/sections/${no}.vue`) // 載入
 			.catch(handleSectionCompMissing),
@@ -86,13 +87,13 @@ function handleSectionCompMissing() { // 當區塊組件載入失敗時
 	console.error( // 在 console 報錯
 		`Section comp is not exist. ${getErrorSectionMessage()}\n`+
 		`-> Check if src/exam-db/${uni}/${year}/sections/${no}.vue exist?\n`+
-		`-> If ${no}.vue exist, check the "${no}" is in section: [...] in ${getErrorConfigPath()}`
+		`-> If ${no}.vue exist, check the "${no}" is in sectionFileBaseNames: [...] in ${getErrorConfigPath()}`
 	);
 	return SectionNotFoundComp;
 }
 
 function getErrorSectionMessage() { // 錯誤發生在哪一題的訊息
-	return `(problem ${props.no} in exam ${props.uni}-${props.year})`;
+	return `(section ${props.no} in exam ${props.uni}-${props.year})`;
 }
 
 function getErrorConfigPath() { // 有錯的設定檔路徑
@@ -102,122 +103,61 @@ function getErrorConfigPath() { // 有錯的設定檔路徑
 
 // #region 若顯示模式為 1, 會顯示綠框答案和詳解連結
 const isLinkMode = () => (props.displayMode === 1 && props.no[0] != "-"); // 若 section base 開頭為 "-", 不會顯示答案和連結
+
 const answerLatex = computed(() => {
 	const s_latex = props.problemConfig.answerLatex; // 題目的答案 (latex 字串)
 	return s_latex ? s_latex : "?"; // 如果讀取不到, 預設為 "?"
 });
-const router = useRouter(); // 路由器
 // #endregion
 
 // #region 若顯示模式為 2, 會顯示綠框答案和詳解
 const isContentMode = () => (props.displayMode === 2 && props.no[0] != '-'); // 顯示模式為 2
-// #endregion
 
-// -------------- refactor line ---------------
-/*
+const contentAsyncComps = shallowRef([]); // 內容組件 ; 避免 async 造成的效能問題, 使用 shallowRef
 
-const problemAsyncComp = shallowRef(null); // 題目的動態組件
-const answerLatex = ref("?"); // 答案的 latex 語法, 預設值為 "?"
-const contentAsyncComps = shallowRef([]); // 內容區塊的動態組件
-
-watch(() => props.problemConfig, async () => { // 當題目改變時, 載入題目和內容區塊的組件
-	problemAsyncComp.value = defineAsyncComponent({
-		loader: () => import(`../exam/${props.uni}/${props.year}/problem/${props.no}.vue`)
-			.catch(handleProblemCompMissing), // 載入題目的組件
-		errorComponent: ProblemNotFoundComp
-	});
+watch(() => [props.uni, props.year, props.no], async () => { // 題號改變時, 載入題目的解答和其他內容
+	if (props.displayMode !== 2) return []; // 顯示模式必須是 2, 才會載入內容區塊組件
 	
-	if (props.no[0] == "-") return; // 題號開頭若為 '-', 會被視為是題本的說明區塊, 只有題目區塊
-	if (!props.problemConfig) { // 如果題目設定檔不存在, 報錯並取消載入答案和內容區塊
-		handleProblemConfigMissing();
-		return;
-	}
-	
-	loadAnswerLatex(); // 載入答案的 latex
-	loadContents(); // 載入內容區塊的組件
-}, { immediate: true });
-
-function loadAnswerLatex() { // 載入答案的 latex
-	const ansLatex = props.problemConfig.answerLatex; // 題目設定檔 (config.problem.<no>) 的答案 (answerLatex)
-	
-	if (ansLatex === undefined) { // key answerLatex 不存在於題目設定檔 (config.problem.<no>) 內
-		handleAnswerUndefined();
-		return;
-	}
-	if (typeof ansLatex !== "string" || ansLatex === "") { // answerLatex 的值應為長度 > 0 的字串
-		handleAnswerError();
-		return;
-	}
-	
-	answerLatex.value = ansLatex;
-}
-
-async function loadContents() { // 載入內容區塊的組件
-	if (props.displayMode != "content") return; // 顯示模式必須是 content, 才會載入內容區塊組件
-	
-	const contentConfig = props.problemConfig.content; // 題目的內容區塊的設定 (config.problem.<no>.content)
-	if (contentConfig === undefined) { // key content 不存在於題目設定檔 (config.problem.<no>) 內
-		handleContentEmpty();
-		return;
-	}
-	if (contentConfig.length === 0) { // config.problem.<no>.content 必須為 [ { "type": ?, "id": ? }, ... ]
-		handleContentEmpty();
-		return;
-	}
+	const contentConfigs = props.problemConfig.contentConfigs; // 題目的內容區塊的設定
+	if (contentConfigs === undefined || contentConfigs.length === 0) return handleContentEmpty();
 	
 	contentAsyncComps.value = await Promise.all( // 載入內容區塊的組件 (內容區塊有可能是解答, 文字區塊等等...)
-		contentConfig.map(async (contentData) => defineAsyncComponent({
-			loader: () => import(`../exam/${props.uni}/${props.year}/content/${contentData.id}.vue`)
-				.catch(error => handleContentMissing(contentData.id)), // 內容區塊組件載入失敗時, 顯示錯誤訊息組件
+		contentConfigs.map(async (contentConfig) => defineAsyncComponent({
+			loader: () => import(`../../exam-db/${props.uni}/${props.year}/contents/${contentConfig.fileBaseName}.vue`)
+				.catch(() => handleContentMissing(contentConfig.fileBaseName)), // 內容區塊組件載入失敗時, 顯示錯誤訊息組件
 			errorComponent: ContentNotFoundComp
 		}))
 	);
-}
+});
 
-function handleProblemCompMissing() { // 題目組件載入失敗時, 顯示錯誤訊息組件
-	console.error( // 在 console 報錯
-		`Problem comp is not exist. ${_getErrorProblemMessage()}\n`+
-		`-> Check if @/components/exam/${props.uni}/${props.year}/problem/${props.no}.vue exist?\n`+
-		`-> If ${props.no}.vue exist, check the "${props.no}" is in section: [...] in ${_getErrorConfigPath()}`
-	);
-	return ProblemNotFoundComp; // 回傳錯誤訊息組件
-}
-
-function handleProblemConfigMissing() { // 題目設定檔不存在
+function handleContentEmpty() { // key content 不存在於題目設定檔 (config.problem.<no>) 內, 回傳空的 promise arr
 	console.error(
-		`Problem config is not exist. ${_getErrorProblemMessage()}\n`+
-		`-> Add "${props.no}": {} in problem: {...} in ${_getErrorConfigPath()}`
+		`Problem contents is undefined or empty. ${getErrorSectionMessage()}\n`+
+		`-> Add "contentConfigs": [ { "type": ?, "id": ? }, ... ] in `+
+		`problemConfigs.${props.no}: {...} in ${getErrorConfigPath()}`
 	);
-}
-
-
-
-function handleContentEmpty() { // key content 不存在於題目設定檔 (config.problem.<no>) 內
-	console.error(
-		`Problem contents is undefined or empty. ${_getErrorProblemMessage()}\n`+
-		`-> Add "content": [ { "type": ?, "id": ? }, ... ] in problem.${props.no}: {...} in ${_getErrorConfigPath()}`
-	);
+	return [];
 }
 
 function handleContentMissing(contentId) { // 內容區塊組件載入失敗時
+	const { uni, year, no } = props;
 	console.error(
-		`Content comp is not exist. ${_getErrorProblemMessage()}\n`+
-		`-> Check if @/components/exam/${props.uni}/${props.year}/content/${contentId}.vue exist?\n`+
-		`-> If ${contentId}.vue exist, check the elements in problem.${props.no}.content: [...] in ${_getErrorConfigPath()} ,`+
-		` and one of element.id must be "${contentId}".`
+		`Content comp is not exist. ${getErrorSectionMessage()}\n`+
+		`-> Check if src/exam-db/${uni}/${year}/contents/${contentId}.vue exist?\n`+
+		`-> If ${contentId}.vue exist, check the elements in `+
+		`problemConfigs.${no}.contentConfigs: [...] in ${getErrorConfigPath()} , `+
+		`and one of element.fileBaseName must be "${contentId}".`
 	);
 	return ContentNotFoundComp; // 回傳錯誤訊息組件
 }
-
-
-*/
+// #endregion
 </script>
 
 <style scoped>
 .hide-problem-score:deep(.problem-score) { /* 隱藏或顯示題目的配分 */
 	display: none; /* 題目的 vue 檔內必須使用 *.problem-score 包裹配分才有效果 */
 }
-.problem-view-link { /* 詳解連結按鈕 (為了實現另開分頁, 由 button 改為 RanLink) */
+.problem-view-link { /* 詳解連結按鈕 (為了實現中鍵另開分頁, 由 button 改為 RanLink) */
 	border-radius: 6px;
 	background-color: var(--ts-gray-800);
 	width: 72px; height: 36px;
--- src/views/exam-page/ProblemView.vue
+++ src/views/exam-page/ProblemView.vue
@@ -7,11 +7,8 @@
 			<!-- 題本的連結 -->
 			<div class="ts-content is-dense">
 				<span class="ts-icon is-reply-icon is-end-spaced"></span>
-				<RanLink
-					:to="`#/exam/${uni}-${year}`"
-					@click="() => { globalVar.examScrollProbNo = no; /* 紀錄欲滾動至的題目編號 */ }"
-				>
-					<span>&nbsp;{{ config.uni[uni]?.shortName ?? "" }}</span>
+				<RanLink :to="`#/exam/${uni}-${year}`" @click="">
+					<span>&nbsp;{{ dbConfig.uniConfigs[uni]?.shortName ?? "" }}</span>
 					<span>&nbsp;{{ year ?? "" }}</span>
 				</RanLink>
 			</div>
@@ -21,11 +18,8 @@
 		<!-- 右側的題目區域 -->
 		<template #content>
 			<div class="ts-content">
-				<Problem v-if="examConfig.problem"
-					:uni="uni" :year="year" :no="no"
-					:problemConfig="examConfig.problem[no]"
-					displayMode="content"
-				></Problem>
+				<Problem :uni="uni" :year="year" :no="no" :problemConfig="problemConfig" :displayMode="2">
+				</Problem>
 			</div>
 		</template>
 		
@@ -33,56 +27,50 @@
 </template>
 
 <script setup>
-import { ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import { globalStore } from "@/store/global"; // pinia 全域變數
+import dbConfig from "@/exam-db/config.json"; // 保存所有題本資訊的設定檔
 import BodyLayout from "@/components/BodyLayout.vue"; // 用於建構 body 的 sidebar 與內容
 import Problem from "@/components/problem/Problem.vue"; // 用於顯示題目與解答的組件
-import config from "@/components/exam/config.json"; // 保存所有題本資訊的設定檔
 
-const globalVar = globalStore(); // 全域變數
-
+// #region 路由解碼
 const route = useRoute(); // 目前的路由資訊
 const router = useRouter(); // 路由器
-
-const uni = ref(undefined); // 學校
-const year = ref(undefined); // 年份
-const no = ref(undefined); // 題號
+const uni = ref(); // 題本的學校英文縮寫
+const year = ref(); // 題本的民國年份
 const examConfig = ref({}); // 題本設定檔
 
-watch(() => route.params.id, async (newExamId) => { // 當路由的題本 id 改變時, 嘗試解碼題本 id
-	var idParam = newExamId.split("-"); // 若路由為 exam/ntu-112, 則 id = "ntu-112", 以 "-" 字符拆分 id
-	if (idParam.length != 2) { // 如果題本 id 的參數個數不為 2, 視為無效 id, 轉址回題本清單
+watch(() => route.params.id, async (newExamId) => { // 當路由改變時, 嘗試解碼題本 id
+	var idParams = newExamId.split("-"); // 若路由為 exam/ntu-112, 則 id = "ntu-112", 以 "-" 字符拆分 id
+	if (idParams.length != 2) { // 如果題本 id 的形式不是 "xxx-xxx", 視為無效 id, 轉址回題本清單
 		handleWrongExamIdFormat(newExamId);
-		return;
+		return; // 提前終止
 	}
-	const [_uni, _year] = idParam; // 題本 id 的第一個參數為 uni, 第二個參數為 year
+	const [_uni, _year] = idParams; // 題本 id 的第一個參數為 uni, 第二個參數為 year
 	
-	const configFile = await import(`../../components/exam/${_uni}/${_year}/config.json`) // 讀取題本設定檔
-		.catch(() => handleExamMissing(_uni, _year)); // 若題本設定檔不存在或路徑錯誤, 報錯, 並轉址回題本清單
-	if (!configFile) return;
+	const examConfigFile = await import(`../../exam-db/${_uni}/${_year}/config.json`) // 讀取題本設定檔
+		.catch(() => handleExamMissing(_uni, _year)) // 若題本設定檔不存在或路徑錯誤, 報錯, 並轉址回題本清單
+	if (!examConfigFile) return; // 題本設定檔不存在, 提前終止
 	
-	uni.value = _uni;
+	uni.value = _uni; // 如果題本設定檔載入成功, 更新學校和年份和題本設定檔
 	year.value = _year;
-	examConfig.value = configFile.default; // json -> Object
+	examConfig.value = examConfigFile.default; // json -> Object
 }, { immediate: true }); // 組件載入時, 做一次
 
-function handleWrongExamIdFormat(wrongExamId) { // 如果題本 id 的參數個數不為 2, 視為無效 id
-	console.error(
-		`Wrong id format. (exam id: ${wrongExamId})\n`
-	);
+function handleWrongExamIdFormat(wrongExamId) { // 如果題本 id 的形式不是 "xxx-xxx", 視為無效 id
+	console.error(`Wrong exam id format "${wrongExamId}".\n`);
 	router.push("/exam"); // 轉址回題本清單
 }
 
-const handleExamMissing = (_uni, _year) => { // 若題本設定檔不存在或路徑錯誤
+function handleExamMissing(_uni, _year) { // 若題本設定檔不存在或路徑錯誤
 	console.error(
-		`Exam config is not exist. (exam ${_uni}-${_year})\n`+
-		`-> Check if @/components/exam/${_uni}/${_year}/config.json exist?\n`
+		`Exam config is not exist. (exam "${_uni}-${_year}")\n`+
+		`-> Check if src/exam-db/${_uni}/${_year}/config.json exist?\n`
 	);
 	router.push("/exam"); // 轉址回題本清單
 };
+// #endregion
 
-watch(() => route.params.prob, (newProblemNo) => { // 當路由的題目編號改變時
-	no.value = newProblemNo;
-}, { immediate: true }); // 組件載入時, 做一次
+const no = computed(() => route.params.prob); // 當路由的題目編號改變時
+const problemConfig = computed(() => examConfig.value.problemConfigs?.[no.value]); // 題目設定
 </script>
