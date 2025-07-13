662ef534f3103423e22263995116d5e33392552a
runnywolf <71980953+runnywolf@users.noreply.github.com>
2025-07-13 16:30:13 +0800

    v0.4.1-dev.3 (comp VueKatex, decode $...$)

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ import Content from "./components/global/Content.vue"; // 內容區塊組件
 import RanLink from "./components/global/RanLink.vue"; // ran 的超連結
 import MakeProblem from "./components/problem/MakeProblem.vue"; // 標準的題目 vue 檔樣式
 import MultiOption from "./components/problem/MultiOption.vue"; // 摺疊的多選題詳解
-import Vl from "./components/problem/Vl.vue"; // 縮短 vatex 標籤長度的組件
+import vl from "./components/problem/Vl.vue"; // 顯示單個 katex 語法
+import vk from "./components/problem/VueKatex.vue"; // 將 slot 內的 $...$ $$...$$ 解碼成
 
 const app = createApp(App);
 
@@ -19,6 +20,7 @@ app.component("Content", Content);
 app.component("RanLink", RanLink);
 app.component("MakeProblem", MakeProblem);
 app.component("MultiOption", MultiOption);
-app.component("vl", Vl);
+app.component("vl", vl);
+app.component("vk", vk);
 
 app.mount("#app");
