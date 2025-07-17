945f7150e7d0cf79062e28079fbf9bf0656e0330
runnywolf <71980953+runnywolf@users.noreply.github.com>
2025-07-17 20:15:19 +0800

    add ntu-109 problem

---
+++ src/exam-db/ntu/109/sections/10.vue
@@ -0,0 +1,20 @@
+<template>
+	<vk>
+		<MakeProblem scoreText="(10%)">
+			<template #problem>
+				If
+				$$
+				A = \begin{bmatrix}
+				0 & -0.5 & 0 & 0 & 0 \\
+				0 & 0 & 0 & 0 & 0 \\
+				0.5 & 0 & 0 & 0 & -0.5 \\
+				0 & 0 & 0 & -1 & 0 
+				\end{bmatrix} ,
+				$$
+				then the numbers of elements $-2, -1, 0, 1, 2$ in a matrix $B$ with
+				$$ \begin{split} ABA ~&=~ A \\ BAB ~&=~ B \\ (AB)^T ~&=~ AB \\ (BA)^T ~&=~ BA. \end{split} $$
+				are ____, ____, ____, ____, ____, respectively.
+			</template>
+		</MakeProblem>
+	</vk>
+</template>
---
+++ src/exam-db/ntu/109/sections/6.vue
@@ -0,0 +1,11 @@
+<template>
+	<vk>
+		<MakeProblem scoreText="(10%)">
+			<template #problem>
+				Prove the following inequality:
+				$$\binom{n}{\lfloor n/2 \rfloor} \ge \frac{2^n}{n}$$
+				where $2 \le n$.
+			</template>
+		</MakeProblem>
+	</vk>
+</template>
