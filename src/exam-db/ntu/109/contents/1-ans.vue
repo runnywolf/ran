41cf63e38d58dbc69c2cb6dbb234bf38acdbb50a
runnywolf <71980953+runnywolf@users.noreply.github.com>
2025-08-07 00:28:44 +0800

    v0.5-dev.6 (tag mapping; add 4 tags for test)

--- src/exam-db/ntu/109/contents/1-ans.vue
+++ src/exam-db/ntu/109/contents/1-ans.vue
@@ -1,7 +1,7 @@
 <template>
 	<vk>
-		$a \le x_i \le b$，生成多項式為 $x^a + x^{a+1} + \cdots + x^b$；有 n 項 $x_i$ 相加則需要 n 次方，<br>
-		因此生成多項式為
+		$a \le x_i \le b$，生成函數為 $x^a + x^{a+1} + \cdots + x^b$；有 n 項 $x_i$ 相加則需要 n 次方，<br>
+		因此生成函數為
 		$$(x^a + x^{a+1} + \cdots + x^b)^n$$
 		，嘗試化簡：
 		$$
@@ -12,6 +12,6 @@
 		&= \boxed{ (1 - x^{b-a+1})^n (1 - x)^{-n} x^a }
 		\end{split}
 		$$
-		寫出任意一個封閉形式的生成多項式應該都算對。
+		寫出任意一個封閉形式的生成函數應該都算對。
 	</vk>
 </template>
--- src/views/exam-page/problem-comp/ProblemTags.vue
+++ src/views/exam-page/problem-comp/ProblemTags.vue
@@ -1,22 +1,19 @@
 <template>
 	<div v-if="tags.length > 0" class="ts-grid is-compact">
-		<div v-for="tag in tags" class="ts-chip is-circular is-start-icon">
+		<div v-for="tag in tags" class="ts-chip is-circular is-start-icon" :data-tooltip="tagMap[tag]?.en">
 			<span class="ts-icon is-hashtag-icon is-small tag-icon"></span>
-			<span>{{ tagMap[tag] ?? "?" }}</span>
+			<span>{{ tagMap[tag]?.zhtw ?? "未知" }}</span>
 		</div>
 	</div>
 	<div v-else class="ts-content is-center-aligned is-fitted">無標籤</div>
 </template>
 
 <script setup>
+import tagMap from "@/exam-db/tag-map.json"; // 用於將 tag 縮寫映射到英文/中文全名
+
 const props = defineProps({
 	tags: { type: Array, default: [] }, // 題目的標籤
 });
-
-const tagMap = { // 將 problem config 當中的多個英文 tag 轉換成中文
-	"recurrence": "遞迴",
-	"generating-func": "生成函數",
-};
 </script>
 
 <style scoped>
