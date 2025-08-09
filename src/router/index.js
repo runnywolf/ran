7f667646027adbe4e850ebd894dd6669b0638362
runnywolf <71980953+runnywolf@users.noreply.github.com>
2025-08-09 23:01:26 +0800

    v0.5-dev.13 (remove router name)

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
-	{ path: "/", name: "Home", component: () => import("@/views/HomeView.vue") },
-	{ path: "/notes", name: "Notes", component: () => import("@/views/NotesView.vue") },
-	{ path: "/exam", name: "ExamMenu", component: () => import("@/views/exam-page/ExamMenuView.vue") },
-	{ path: "/exam/:id", name: "Exam", component: () => import("@/views/exam-page/ExamView.vue") }, // 題本編號 :id 的格式範例: "ntu-110"
-	{ path: "/exam/:id/:prob", name: "Problem", component: () => import("@/views/exam-page/ProblemView.vue") },
+	{ path: "/", component: () => import("@/views/HomeView.vue") },
+	{ path: "/notes", component: () => import("@/views/NotesView.vue") },
+	{ path: "/exam", component: () => import("@/views/exam-page/ExamMenuView.vue") },
+	{ path: "/exam/:id", component: () => import("@/views/exam-page/ExamView.vue") }, // 題本編號 :id 的格式範例: "ntu-110"
+	{ path: "/exam/:id/:prob", component: () => import("@/views/exam-page/ProblemView.vue") },
 	{ path: "/search", component: () => import("@/views/SearchView.vue") },
 	{ path: "/search/:tag", component: () => import("@/views/SearchView.vue") }, // 會自動新增一個 tag 在搜尋欄下方
 	{
-		path: "/practice", name: "Practice", component: () => import("@/views/PracticeView.vue"),
+		path: "/practice", component: () => import("@/views/PracticeView.vue"),
 		redirect: "/practice/recur",
 		children: [
 			{ path: "recur", component: () => import("@/views/practice-page/RecurView.vue") },
@@ -23,14 +23,14 @@ const routes = [
 		]
 	},
 	{
-		path: "/other", name: "Other", component: () => import("@/views/OtherView.vue"),
+		path: "/other", component: () => import("@/views/OtherView.vue"),
 		redirect: "/other/stat",
 		children: [
 			{ path: "stat", component: () => import("@/views/other-page/StatView.vue") },
 			{ path: ":pathMatch(.*)", redirect: "/other" },
 		]
 	},
-	{ path: "/test", name: "Test", component: () => import("@/tests/TestView.vue") , redirect: "/" }, // 測試用
+	{ path: "/test", component: () => import("@/tests/TestView.vue") , redirect: "/" }, // 測試用
 	{ path: "/:pathMatch(.*)", redirect: "/" },
 ];
 
