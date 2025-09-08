081e0b0fe4c4fa418da85cacd5335ca4b7326c83
runnywolf <71980953+runnywolf@users.noreply.github.com>
2025-09-08 21:21:55 +0800

    Ciallo ~ (∠・ω < )⌒☆

--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -3,7 +3,9 @@
 		<div class="ts-content is-very-padded">
 			<div class="ts-wrap is-vertical is-middle-aligned is-compact">
 				<span class="ts-icon is-hammer-icon is-huge is-spinning"></span>
-				<div class="ts-text is-huge">施工中...&nbsp;&nbsp;ฅ^⦁⩊⦁^ฅ ੭</div>
+				<div class="ts-text is-huge">
+					施工中...&nbsp;&nbsp;<span class="ran-neko"></span>
+				</div>
 			</div>
 		</div>
 	</div>
