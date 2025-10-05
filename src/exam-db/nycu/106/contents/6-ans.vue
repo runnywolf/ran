19a635ed80845174c0106022ba91d5acaf6b7495
runnywolf <71980953+runnywolf@users.noreply.github.com>
2025-10-05 20:56:33 +0800

    nycu recur ans

--- src/exam-db/nycu/106/contents/6-ans.vue
+++ src/exam-db/nycu/106/contents/6-ans.vue
@@ -1,5 +1,7 @@
 <template>
-	<vk>
-		第 6 題的解答
-	</vk>
+	<Recur :recurCoef="[4, -4]" :initConst="[4, 4]"></Recur>
 </template>
+
+<script setup>
+import Recur from "@/components/answer-generator/Recur.vue"; // 遞迴解答產生器
+</script>
--- src/exam-db/nycu/110/contents/10-ans.vue
+++ src/exam-db/nycu/110/contents/10-ans.vue
@@ -1,5 +1,27 @@
 <template>
 	<vk>
-		第 10 題的解答
+		<MultiOption :optionNames="[ 'a', 'b', 'c' ]">
+			<template #a>
+				
+			</template>
+			<template #b>
+				<Recur :recurCoef="[-2, 2, 4]" :initConst="[7, -6, 20]"></Recur>
+				最後求 $G_{100}$：
+				$$
+				\begin{split}
+				G_{100} &= 3(-2)^{100} + 2(\sqrt{2})^{100} + 2(-\sqrt{2})^{100} \\
+				&= 3 \cdot 2^{100} + 2 \cdot 2^{50} + 2 \cdot 2^{50} \\
+				&= \boxed{ 3 \cdot 2^{100} + 4 \cdot 2^{50} }
+				\end{split}
+				$$
+			</template>
+			<template #c>
+				
+			</template>
+		</MultiOption>
 	</vk>
 </template>
+
+<script setup>
+import Recur from "@/components/answer-generator/Recur.vue"; // 遞迴解答產生器
+</script>
