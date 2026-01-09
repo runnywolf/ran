6f388cad6cfd6a2c1701ee49f902eddfcd970f04
runnywolf <71980953+runnywolf@users.noreply.github.com>
2026-01-09 15:17:53 +0800

    v0.6-dev.14 (add top row in Problem.vue)
    move .hide-problem-score to Problem.vue

--- src/components/problem/Problem.vue
+++ src/components/problem/Problem.vue
@@ -1,26 +1,43 @@
 <!-- 用來顯示題目的組件, 會附帶很多 ui 可以選擇要不要顯示
 
-| topRow           | 包含收藏按鈕, UniYearNo 藍框, 多個標籤的 row
-
 這些組件需要顯示的 ui:
 | ExamPaper (exam mode)   |
-| ExamPaper (!exam mode)  | showAnswer showLink showStar
-| ProblemView             | hideProblemScore showAnswer showContent showStar
-| SearchResult, SavedPage | hideProblemScore showLink topRow[showStar showUniYearNo showTags]
-| NotePage (future)       | hideProblemScore showLink topRow[showStar showUniYearNo showTags]
+| ExamPaper (!exam mode)  | showAnswer showLink
+| ProblemView             | hideProblemScore showAnswer showContent
+| SearchResult, SavedPage | hideProblemScore showLink topRow
+| NotePage (future)       | hideProblemScore showLink topRow
 -->
 
 <template>
 	<div class="ts-wrap is-vertical" :class="{ 'hide-problem-score': hideProblemScore }">
+		
+		<!-- topRow: 包含收藏按鈕, 題號, 題目的多個標籤 -->
+		<div v-if="showTopRow" class="ts-grid">
+			
+			<!-- 收藏按鈕 -->
+			<SaveButton :uni="uni" :year="year" :no="no" />
+			
+			<!-- 題號 -->
+			<div class="column problem-name ran-app-font">
+				<span>{{ getUniShortName(uni) }} {{ year }} 第 {{ no }} 題</span>
+			</div>
+			
+			<!-- 題目的多個標籤 -->
+			<div class="column is-fluid ts-grid is-top-aligned" style="row-gap: 10px;">
+				<Tag v-for="tag in (problemConfig.tags ?? [])" :tag="tag"></Tag>
+			</div>
+			
+		</div>
 		
 		<!-- 題目 -->
 		<div class="ran-problem-font">
 			<component :is="sectionComp"></component>
 		</div>
 		
+		<!-- 答案框, 詳解連結, 收藏按鈕, 內容區塊(詳解) -->
 		<div v-if="props.no && props.no[0] !== '-' && (showAnswer || showLink || showContent)"
 			class="ts-wrap is-compact is-vertical content"
-		><!-- 開頭為 "-" 的 section 不是題目, 無詳解 -->
+		><!-- 題號開頭為 "-" 的 section 不是題目, 不顯示這個區塊的答案詳解 -->
 			
 			<!-- 綠色答案框 & 詳解連結 -->
 			<div class="ts-wrap">
@@ -59,8 +76,10 @@
 
 <script setup>
 import { shallowRef, watchEffect, defineAsyncComponent } from "vue";
-import { getProblemConfig, getSectionComp, getAllContentComps } from "@lib/exam-db"; // 讀取題本資訊
+import { getUniShortName, getProblemConfig, getSectionComp, getAllContentComps } from "@lib/exam-db"; // 讀取題本資訊
 import AnswerBox from "./problem-comp/AnswerBox.vue"; // 綠色答案框的組件
+import SaveButton from "../global/SaveButton.vue"; // 收藏按鈕
+import Tag from "./Tag.vue"; // 標籤
 import LoadingComp from "./problem-comp/Loading.vue"; // 題目加載組件
 import SectionNotFoundComp from "./problem-comp/SectionNotFound.vue"; // 區塊載入失敗時, 顯示的錯誤訊息組件
 import ContentNotFoundComp from "./problem-comp/ContentNotFound.vue"; // 題目的內容載入失敗時, 顯示的錯誤訊息組件
@@ -71,6 +90,7 @@ const props = defineProps({
 	no: { type: String, default: null }, // 題號
 	problemConfig: { type: Object, default: null }, // 題目的設定檔, 位於 config.problemConfigs.<no> 內, 若此值不傳會自己去抓
 	hideProblemScore: { type: Boolean, default: false }, // 隱藏題目的配分. 因為配分已存在於題目組件檔, 所以只有這個參數是 hide (某種定義?)
+	showTopRow: { type: Boolean, default: false }, // 是否顯示 top row, 包含收藏按鈕, 題號, 題目的多個標籤
 	showAnswer: { type: Boolean, default: false }, // 顯示綠框答案
 	showLink: { type: Boolean, default: false }, // 顯示 "詳解" 按鈕
 	showContent: { type: Boolean, default: false }, // 顯示內容區塊 (包含詳解)
@@ -114,6 +134,16 @@ watchEffect(async () => {
 </script>
 
 <style scoped>
+.hide-problem-score:deep(.problem-score) { /* 用於控制題目的配分是否要顯示 */
+	display: none;
+}
+.problem-name > span { /* 藍色框的題目名稱 */
+	display: inline-block;
+	padding: 0 6px;
+	background-color: #bdf;
+	border-radius: 4px;
+	font-size: 15px;
+}
 .problem-view-link { /* 詳解連結按鈕 (為了實現中鍵另開分頁, 由 button 改為 RanLink) */
 	border-radius: 6px;
 	background-color: var(--ts-gray-800);
