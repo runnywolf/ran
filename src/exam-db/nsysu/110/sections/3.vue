23217527f928f1ad6d82eb1d4d14ad9c3de67711
runnywolf <71980953+runnywolf@users.noreply.github.com>
2026-02-13 17:52:24 +0800

    nsysu-110 problem

---
+++ src/exam-db/nsysu/110/sections/3.vue
@@ -0,0 +1,9 @@
+<template>
+	<MakeProblem listEndLabel="b" :listItemScoreTexts="['[10%]', '[10%]']">
+		<template #a>
+			Prove that if 169 integers are selected from $\{1, 2, 3, \cdots\!, 336\}$,
+			then the selection must include two integers $x, y$, where $x \mid y$ or $y \mid x$.
+		</template>
+		<template #b>Write a statement that generalizes the results of part (a).</template>
+	</MakeProblem>
+</template>
---
+++ src/exam-db/nsysu/110/sections/7.vue
@@ -0,0 +1,5 @@
+<template>
+	<MakeProblem scoreText="[10%]">
+		<template #problem>Find $23^{-1}$ in $\mathbb{Z}_{82}$ by Euclidean algorithm.</template>
+	</MakeProblem>
+</template>
