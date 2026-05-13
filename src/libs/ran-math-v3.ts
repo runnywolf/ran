8c6183be520b0a96b5cb1363ada5af2841fce1a9
runnywolf <71980953+runnywolf@users.noreply.github.com>
2026-05-13 12:19:55 +0800

    SolveQuad

--- src/libs/ran-math-v3.ts
+++ src/libs/ran-math-v3.ts
@@ -930,4 +930,25 @@ export class Matrix<T extends MatrixElement<T>> { // 矩陣
 	}
 }
 
+export class SolveQuad { // 解二次方程式
+	readonly roots: [Scalar, Scalar];
+	
+	constructor(a: number|bigint|Frac, b: number|bigint|Frac, c: number|bigint|Frac) { // 解二次方程式 ax^2 + bx + c = 0
+		const A = SC(ParamNorm.toSvOrCp(a, "SolveQuad.constructor"), false);
+		const B = SC(ParamNorm.toSvOrCp(b, "SolveQuad.constructor"), false);
+		const C = SC(ParamNorm.toSvOrCp(c, "SolveQuad.constructor"), false);
+		const center = B.neg().div(2n).div(A); // -b/2a
+		const squareOffset = center.mul(center).sub(C.div(A)); // (-b/2a)^2 - c/a
+		const offset = SqrtValue.is(squareOffset.value) ? // √( (-b/2a)^2 - c/a )
+			SV([1n, squareOffset.value.terms.get(1n) ?? F(0)]) : // 因為是有理數, 所以直接把 √1 基底取出, 開根號
+			squareOffset.pow(0.5); // 浮點數就直接 ^ 0.5 即可
+		
+		this.roots = [center.sub(offset), center.add(offset)]; // x = -b/2a ± √( (-b/2a)^2 - c/a )
+	}
+	
+	toStr(): string { // 轉為字串
+		return this.roots.map(root => root.toStr()).join(" , ");
+	}
+}
+
 export const __test__ = { Prime }; // only for test
